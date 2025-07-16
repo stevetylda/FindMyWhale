d1961eaa2227cb9bbc3e1ec49792af671f51cdf3
Tyler <stevetylda@gmail.com>
2025-07-16 10:48:36 -0400

    Added Streamlit Page Configuration

--- examples/streamlit_demo_application/app.py
+++ examples/streamlit_demo_application/app.py
@@ -1,4 +1,22 @@
+# Import Modules
 import streamlit as st
+
+# Set Page Configuration
+st.set_page_config(
+    page_title="FindMyWhale - Orca Localization",
+    page_icon=":material/near_me:",
+    layout="wide",
+)
+
+hide_default_format = """
+       <style>
+       #MainMenu {visibility: hidden; }
+       footer {visibility: hidden;}
+       </style>
+       """
+st.markdown(hide_default_format, unsafe_allow_html=True)
+
+st.header(":whale: FindMyWhale", divider="gray")
 
 
 st.write("Test")
